943a17d459a5964173e3c4024396be5ad903f87d
AsierBascones <asier.bascones@opendeusto.es>
2026-03-06 09:50:08 +0100

    PositionLogTrace Method Added

---
+++ Assets/PositionTracker.cs
@@ -0,0 +1,26 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PositionTracker : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PositionLogTrace();
+    }
+
+    private void PositionLogTrace()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Vector3 posicion = transform.position;
+
+            Debug.Log("Posición de " + gameObject.name + ": " + posicion);
+        }
+    }
+}
